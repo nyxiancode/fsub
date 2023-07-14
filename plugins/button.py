88b8527b311c7edb2e87cdad4ba8827f2200c099
kaitod-id <108166655+kaitod-id@users.noreply.github.com>
2023-07-14 15:05:04 +0700

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -3,13 +3,12 @@ from pyrogram.types import InlineKeyboardButton
 
 
 def start_button(client):
-    buttons = [
-        [
-            InlineKeyboardButton(text="ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅs", callback_data="help"),
-        ],
-    ]
+    buttons = []
 
-    if not FORCE_SUB_CHANNEL and not FORCE_SUB_GROUP and not FORCE_SUB_CHANNEL2:
+    if not FORCE_SUB_CHANNEL2 and not FORCE_SUB_GROUP:
+        buttons.append([
+            InlineKeyboardButton(text="ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅs", callback_data="help"),
+        ])
         buttons.append([
             InlineKeyboardButton(text="ᴛᴜᴛᴜᴘ", callback_data="close"),
         ])
@@ -17,6 +16,9 @@ def start_button(client):
     if not FORCE_SUB_CHANNEL and FORCE_SUB_GROUP and not FORCE_SUB_CHANNEL2:
         buttons.insert(0, [
             InlineKeyboardButton(text="ɢʀᴏᴜᴘ", url=client.invitelink2),
+        ])
+        buttons.append([
+            InlineKeyboardButton(text="ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅs", callback_data="help"),
         ])
         buttons.append([
             InlineKeyboardButton(text="ᴛᴜᴛᴜᴘ", callback_data="close"),
@@ -27,22 +29,27 @@ def start_button(client):
             InlineKeyboardButton(text="ᴄʜᴀɴɴᴇʟ", url=client.invitelink),
         ])
         buttons.append([
-            InlineKeyboardButton(text="ᴛᴜᴛᴜᴘ", callback_data="close"),
-        ])
-
-    if FORCE_SUB_CHANNEL and FORCE_SUB_GROUP and not FORCE_SUB_CHANNEL2:
-        buttons.insert(0, [
             InlineKeyboardButton(text="ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅs", callback_data="help"),
-        ])
-        buttons.insert(0, [
-            InlineKeyboardButton(text="ᴄʜᴀɴɴᴇʟ", url=client.invitelink),
-            InlineKeyboardButton(text="ɢʀᴏᴜᴘ", url=client.invitelink2),
         ])
         buttons.append([
             InlineKeyboardButton(text="ᴛᴜᴛᴜᴘ", callback_data="close"),
         ])
 
-    if FORCE_SUB_CHANNEL2 and not FORCE_SUB_GROUP:
+    if FORCE_SUB_CHANNEL and FORCE_SUB_GROUP and not FORCE_SUB_CHANNEL2:
+        buttons.insert(0, [
+            InlineKeyboardButton(text="ɢʀᴏᴜᴘ", url=client.invitelink2),
+        ])
+        buttons.insert(0, [
+            InlineKeyboardButton(text="ᴄʜᴀɴɴᴇʟ", url=client.invitelink),
+        ])
+        buttons.append([
+            InlineKeyboardButton(text="ʜᴇʟᴘ & ᴄᴏᴍᴀɴᴅs", callback_data="help"),
+        ])
+        buttons.append([
+            InlineKeyboardButton(text="ᴛᴜᴛᴜᴘ", callback_data="close"),
+        ])
+
+    if FORCE_SUB_CHANNEL2:
         buttons.insert(0, [
             InlineKeyboardButton(text="CHANNEL2", url=client.invitelink3),
         ])
@@ -82,7 +89,11 @@ def fsub_button(client, message):
     if FORCE_SUB_CHANNEL and FORCE_SUB_GROUP and not FORCE_SUB_CHANNEL2:
         buttons.insert(0, [
             InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=client.invitelink),
+        ])
+        buttons.insert(0, [
             InlineKeyboardButton(text="ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅs", callback_data="help"),
+        ])
+        buttons.insert(0, [
             InlineKeyboardButton(text="ɢʀᴏᴜᴘ", url=client.invitelink2),
         ])
 
